fcc1a6151c5d06f65bdde937c99b44df290dd384
viniciustrr <vinicius.ribeiro@ccc.ufcg.edu.br>
2025-09-15 23:35:29 -0300

    pix

---
+++ sections/Pix.tsx
@@ -0,0 +1,34 @@
+import PixIsland from "../islands/PixIsland.Tsx";
+
+interface PixKeyItem {
+    label?: string;
+    value: string;
+    holderName?: string;
+    bankName?: string;
+}
+
+interface Props {
+    title?: string;
+    description?: string;
+    keys: PixKeyItem[];
+    successMessage?: string;
+    errorMessage?: string;
+}
+
+export default function PixSection({
+    title,
+    description,
+    keys,
+    successMessage,
+    errorMessage
+}: Props) {
+    return (
+        <PixIsland
+            title={title}
+            description={description}
+            keys={keys}
+            successMessage={successMessage}
+            errorMessage={errorMessage}
+        />
+    );
+} 
